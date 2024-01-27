3cc7bab27a180981e2d3f6f0e468e2662723c267
vlad <vladspro15@gmail.com>
2024-01-27 10:27:38 +0200

    new game

--- src/GO IT/first game.py
+++ src/GO IT/first game.py
@@ -1,6 +1,6 @@
 import random
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -17,7 +17,12 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+#player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_right = [1, 0]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
+
 
 playing = True
 while playing:
@@ -27,6 +32,21 @@ while playing:
             playing = False
 
     main_display.fill(COLOR_BLACK)
+
+    keys = pygame.key.get_pressed()
+
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
+
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
+
+    if keys[K_LEFT] and player_rect.left > 0:
+        player_rect
+
 
     # if player_rect.bottom >= HEIGHT:
     #     player_speed[1] = -player_speed[1]
@@ -42,6 +62,6 @@ while playing:
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
+    #player_rect = player_rect.move(player_speed)
 
     pygame.display.flip()
